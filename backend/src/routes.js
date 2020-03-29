21bc4576c5d75c6829a9833f76a467025de5a45e
Ângelo Pietro <arodrigues@widepay.com>
2020-03-29 15:51:39 -0300

    chore: primeiro commit

---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const ProfileController = require('./controllers/ProfileController');
+const IncidentController = require('./controllers/IncidentController');
+const SessionController = require('./controllers/SessionController');
+
+const router = express.Router();
+
+router.post('/sessions', SessionController.create);
+
+router.get('/ongs', OngController.index);
+router.post('/ongs', OngController.create);
+
+router.get('/profile', ProfileController.index);
+
+router.get('/incidents', IncidentController.index);
+router.post('/incidents', IncidentController.create);
+router.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = router;
